bb11fe9a60e3cef8a22f3ca12821eb02d24c9dd1
Amanda Herschleb <amanda.herschleb@markit.com>
2019-08-16 08:25:16 -0600

    lesson 1 - the vue instance

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+	// connects to div in html with id app
+	el: "#app",
+	// variables that you want access to in the html
+	data: {
+		product: "Socks",
+		description: "Wool socks to keep your feet warm in the winter."
+	}
+})
