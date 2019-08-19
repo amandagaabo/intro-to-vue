a94355e4b585d42e0e543f678b243e43faaaef90
Amanda Herschleb <amanda.herschleb@markit.com>
2019-08-19 12:01:10 -0600

    lesson 10 - forms

--- main.js
+++ main.js
@@ -1,6 +1,100 @@
 // enable dev tools
 Vue.config.devtools = true;
 
+Vue.component("product-review", {
+	template: `
+		<form class="review-form" @submit.prevent="onSubmit">
+			<p v-if="errors.length">
+				<b>Please correct the following error(s):</b>
+				<ul>
+					<li v-for="error in errors">
+						{{ error }}
+					</li>
+				</ul>
+			</p>
+			<p>
+				<label for="name">
+					Name:
+				</label>
+				<input id="name" v-model="name"/>
+			</p>
+
+			<p>
+				<label for="review">
+					Review:
+				</label>
+				<textarea id="review" v-model="review"/>
+			</p>
+
+			<p>
+				<label for="rating">Rating:</label>
+				<select id="rating" v-model.number="rating">
+					<option>5</option>
+					<option>4</option>
+					<option>3</option>
+					<option>2</option>
+					<option>1</option>
+				</select>
+			</p>
+
+			<p>Would you recommend this product?</p>
+				<label>
+					Yes
+					<input
+						type="radio"
+						value="Yes"
+						v-model="recommend"
+					/>
+				</label>
+				<label>
+					No
+					<input
+						type="radio"
+						value="No"
+						v-model="recommend"
+					/>
+				</label>
+
+			<p>
+				<input type="submit" value="Submit"/>
+			</p>
+		</form>
+	`,
+	data () {
+		return {
+			name: null,
+			review: null,
+			rating: null,
+			recommend: null,
+			errors: []
+		}
+	},
+	methods: {
+		onSubmit () {
+			if (this.name && this.review && this.rating && this.recommend) {
+				let productReview = {
+					name: this.name,
+					review: this.review,
+					rating: this.rating,
+					recommend: this.recommend
+				}
+				// emit event to send to parent
+				this.$emit('review-submitted', productReview)
+				this.name = null
+				this.review = null
+				this.rating = null
+				this.recommend = null
+			}
+			else {
+				if (!this.name) this.errors.push("Name is required.")
+				if (!this.review) this.errors.push("Review is required.")
+				if (!this.rating) this.errors.push("Rating is required.")
+				if (!this.recommend) this.errors.push("Recommend is required.")
+			}
+		}
+	}
+})
+
 Vue.component("product-details", {
 	props: {
 		details: {
@@ -26,89 +120,102 @@ Vue.component("product", {
 	},
 	template: `
 		<div class="product">
-		<div class="product-image">
-			<!-- v-bind is used when you want the attribute (src) to update when data changes -->
-			<!-- shorthand for v-bind is just :attribute -->
-			<!-- attributes: href, alt, title, class, style, disabled, src, etc -->
-			<img v-bind:src="image"/>
-		</div>
-
-		<div class="product-info">
-			<h1>
-				<!-- {{ }} = expression, where you can use Vue data -->
-				{{ title }}
-			</h1>
-
-			<!-- v-if, v-else-if and v-else are used to conditionally display something -->
-			<!-- v-if adds or removes the element from the dom -->
-			<!-- use v-show to toggle visibility if the view will be toggled a lot for better perforance -->
-			<p v-if="inventory > 10">
-				In Stock
-			</p>
-			<p v-else-if="inventory <= 10 && inventory > 0">
-				Almost Gone
-			</p>
-			<p
-				v-else
-				:class="{ outOfStock: inventory === 0 }"
-			>
-				Out of Stock
-			</p>
-
-			<p>
-				Shipping: {{ shipping }}
-			</p>
-
-			<p v-if="onSale">
-				{{ saleMessage }}
-			</p>
-
-			<product-details
-				:details="details"
-			/>
-
-			<ul>
-				<li v-for="size in sizes">
-					{{ size }}
-				</li>
-			</ul>
-
-			<!-- use key with v-bind when looping and creating elements -->
-			<!-- v-on has a short cut: @ -->
-			<!-- can call a method with v-on/@ and pass a property to it -->
-			<!-- :style uses style binding so you can reference data in the style properties -->
-			<!-- can also bind to a styleObject that lives in data instead of listing individual styles -->
-			<div
-				v-for="variant, index in variants"
-				class="color-box"
-				:key="variant.variantId"
-				:style="{ backgroundColor: variant.variantColor }"
-				@mouseover="updateProduct(index)"
-			>
+			<div class="product-image">
+				<!-- v-bind is used when you want the attribute (src) to update when data changes -->
+				<!-- shorthand for v-bind is just :attribute -->
+				<!-- attributes: href, alt, title, class, style, disabled, src, etc -->
+				<img v-bind:src="image"/>
 			</div>
 
-			<!-- v-on is used to listen for then handle events -->
-			<!-- events: click, mouseover, submit, keyup -->
-			<!-- can write expression (itemsInCart += 1) or call a method -->
-			<!-- conditionally add a class by using an object where the key is the class name and value is an expression for if it is added -->
-			<button
-				@click="addToCart"
-				:class="{ disabledButton: inventory === 0 }"
-				:disabled="inventory === 0"
-			>
-				Add to Cart
-			</button>
+			<div class="product-info">
+				<h1>
+					<!-- {{ }} = expression, where you can use Vue data -->
+					{{ title }}
+				</h1>
 
-			<button
-				@click="removeFromCart"
-			>
-				Remove from Cart
-			</button>
+				<!-- v-if, v-else-if and v-else are used to conditionally display something -->
+				<!-- v-if adds or removes the element from the dom -->
+				<!-- use v-show to toggle visibility if the view will be toggled a lot for better perforance -->
+				<p v-if="inventory > 10">
+					In Stock
+				</p>
+				<p v-else-if="inventory <= 10 && inventory > 0">
+					Almost Gone
+				</p>
+				<p
+					v-else
+					:class="{ outOfStock: inventory === 0 }"
+				>
+					Out of Stock
+				</p>
+
+				<p>
+					Shipping: {{ shipping }}
+				</p>
+
+				<p v-if="onSale">
+					{{ saleMessage }}
+				</p>
+
+				<product-details
+					:details="details"
+				/>
+
+				<ul>
+					<li v-for="size in sizes">
+						{{ size }}
+					</li>
+				</ul>
+
+				<!-- use key with v-bind when looping and creating elements -->
+				<!-- v-on has a short cut: @ -->
+				<!-- can call a method with v-on/@ and pass a property to it -->
+				<!-- :style uses style binding so you can reference data in the style properties -->
+				<!-- can also bind to a styleObject that lives in data instead of listing individual styles -->
+				<div
+					v-for="variant, index in variants"
+					class="color-box"
+					:key="variant.variantId"
+					:style="{ backgroundColor: variant.variantColor }"
+					@mouseover="updateProduct(index)"
+				>
+				</div>
+
+				<!-- v-on is used to listen for then handle events -->
+				<!-- events: click, mouseover, submit, keyup -->
+				<!-- can write expression (itemsInCart += 1) or call a method -->
+				<!-- conditionally add a class by using an object where the key is the class name and value is an expression for if it is added -->
+				<button
+					@click="addToCart"
+					:class="{ disabledButton: inventory === 0 }"
+					:disabled="inventory === 0"
+				>
+					Add to Cart
+				</button>
+
+				<button @click="removeFromCart">
+					Remove from Cart
+				</button>
+			</div>
+
+			<div>
+				<h2>Reviews</h2>
+				<p v-if="!reviews.length">There are no reviews yet.</p>
+
+				<ul>
+					<li v-for="review in reviews">
+						<p> {{ review.name }} </p>
+						<p> Rating: {{ review.rating }} </p>
+						<p> Recommend: {{ review.recommend }} </p>
+						<p> {{ review.review }} </p>
+					</li>
+				</ul>
+			</div>
+			<product-review @review-submitted="addReview"></product-review>
 		</div>
-	</div>
 	`,
 	// variables that you want access to in the html
-	data() {
+	data () {
 		return {
 			brand: "Vue Mastery",
 			product: "Socks",
@@ -138,7 +245,8 @@ Vue.component("product", {
 				"S",
 				"M",
 				"L"
-			]
+			],
+			reviews: []
 		}
 	},
 	methods: {
@@ -151,25 +259,28 @@ Vue.component("product", {
 		},
 		updateProduct: function (index) {
 			this.selectedVariant = index;
+		},
+		addReview: function (productReview) {
+			this.reviews.push(productReview)
 		}
 	},
 	computed: {
-		title() {
+		title () {
 			return this.brand + " " + this.product
 		},
-		onSale() {
+		onSale () {
 			return this.variants[this.selectedVariant].variantOnSale
 		},
-		inventory() {
+		inventory () {
 			return this.variants[this.selectedVariant].variantQuantity
 		},
-		image() {
+		image () {
 			return this.variants[this.selectedVariant].variantImage
 		},
-		saleMessage() {
+		saleMessage () {
 			return this.brand + " " + this.product + " are on sale!"
 		},
-		shipping() {
+		shipping () {
 			if (this.premium) return "Free"
 			return "$5.99"
 		}
@@ -185,10 +296,10 @@ const app = new Vue({
 		cart: []
 	},
 	methods: {
-		addItem(id) {
+		addItem (id) {
 			this.cart.push(id)
 		},
-		removeItem(id) {
+		removeItem (id) {
 			for(let i = this.cart.length - 1; i >= 0; i--) {
 				if (this.cart[i] === id) {
 					this.cart.splice(i, 1);
